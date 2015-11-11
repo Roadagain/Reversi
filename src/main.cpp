6e2e47ec1ccd62411bd4f1c33704bb26f26998da
Roadagain <ringoh72@gmail.com>
2015-11-11 22:02:30 +0900

    Added debug part of printing board

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <ncurses.h>
 
+#include "board.hpp"
+using namespace roadagain;
+
 int main()
 {
     initscr();
     cbreak();
     noecho();
+
+    Board().print();
+    getch();
 
     endwin();
     return (0);
