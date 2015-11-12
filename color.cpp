9a9d43fc232b92eca90d585b21fa140c27c14f48
Roadagain <ringoh72@gmail.com>
2015-11-12 14:27:56 +0900

    Added missing color number

--- color.cpp
+++ color.cpp
@@ -6,6 +6,8 @@ namespace roadagain
 {
 
 const int Colors::BOARD_NUMBER = 1;
+const int Colors::BLACK_NUMBER = 2;
+const int Colors::WHITE_NUMBER = 3;
 const Color Colors::BOARD = { BOARD_NUMBER, COLOR_BLACK, COLOR_GREEN };
 const Color Colors::BLACK = { BLACK_NUMBER, COLOR_BLACK, COLOR_BLACK };
 const Color Colors::WHITE = { WHITE_NUMBER, COLOR_WHITE, COLOR_WHITE };
