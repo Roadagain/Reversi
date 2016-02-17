44afdaf6e7d7448cdb5d75b2e60cda8d0da3d6ae
Roadagain <ringoh72@gmail.com>
2016-02-17 23:22:28 +0900

    Add log structure

---
+++ log.hpp
@@ -0,0 +1,18 @@
+#ifndef INCLUDE_ROADAGAIN_LOG
+#define INCLUDE_ROADAGAIN_LOG
+
+#include "board.hpp"
+
+namespace roadagain
+{
+
+struct Log
+{
+    int y;
+    int x;
+    BoardState stone;
+};
+
+}
+
+#endif
