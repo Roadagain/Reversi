95d78bdb7e1a6decae5c3f91e1eb3a4770f46bff
Roadagain <ringoh72@gmail.com>
2016-02-17 22:47:56 +0900

    Add level usage

--- config.cpp
+++ config.cpp
@@ -101,6 +101,8 @@ void Config::help()
     std::puts("  --automatic    Play automatically.");
     std::puts("  --color[=WHEN] Colorize the output; ");
     std::puts("                 WHEN can be 'always' (default if omitted), 'auto', or 'never'.");
+    std::puts("  --level=LEVEL  Choose enemy's level;");
+    std::puts("                 LEVEL can be 'easy', 'hard'.");
     std::puts("  --help         Print this help and exit successfully.");
     std::puts("");
     std::puts("Color must be 'black' or 'white'.");
